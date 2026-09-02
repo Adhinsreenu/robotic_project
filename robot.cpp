cbad12357926e130e545f56de4bf5253dbc4531f
adhinmnair <adhinsreenu123@gmail.com>
2026-09-02 09:48:22 +0530

    robot.cpp file added

--- robot.cpp
+++ robot.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
+
+    int battery=30;
+    cout<<"robot is running with battery of "<<battery<<"%"<<endl;
+    return 0;
+
 int main()
 {
     int battery=30;
